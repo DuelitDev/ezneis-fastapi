675e50592d944ff251daad5bfb89eee50af8f10f
DuelitDev <jyoon07dev@gmail.com>
2025-02-07 20:04:04 +0900

    Use localhost as default host for uvicorn.
    Changed the uvicorn host from "0.0.0.0" to "localhost" for increased security and to limit external access during local development. This ensures the application is only accessible from the local machine by default.

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ async def general_exception_handler(request, exc):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+    uvicorn.run(app, host="localhost", port=PORT)
